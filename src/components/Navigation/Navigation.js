a52f32e14e8724d488c85cee71b1b6ffd931a7be
Joe Ishak <jishak@infosol.com>
2018-08-15 13:33:31 -0700

    Fixed conflicts

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,27 +4,45 @@ import  classNames  from 'classnames';
 import styles from './Navigation.css';
 import logo from "../../assets/images/adobe-logo-nav.svg";
 import { Route,Link } from 'react-router-dom';
-
+import {Nav, NavItem} from 'react-bootstrap';
 class Navigation extends Component {
 
     
     render() {
        
         return(
-            <nav className="navContainer navbar navbar-expand-lg navbar-dark bg-dark" >
-            <a className="navbar-brand" href="#">
-              <img className="imgLogo" src={logo}  />
-            </a>
-            <ul>
-                <li> 
-                <Link to="/">Home</Link>
-                </li>
-                <li>
-                <Link to="/post">Post a Comment</Link>
-                </li>
-                
-            </ul>
-          </nav>
+        //     <nav className="navContainer navbar navbar-expand-lg navbar-dark bg-dark" >
+        //     <a className="navbar-brand" href="#">
+        //       <img className="imgLogo" src={logo}  />
+        //     </a>
+        //     <ul>
+        //         <li> 
+        //         <Link to="/">Summary</Link>
+        //         </li>
+        //         <li>
+        //         <Link to="/">Journeys</Link>
+        //         </li>
+        //         <li>
+        //         <Link to="/">LoremIpsum</Link>
+        //         </li>
+        //         <li>
+        //         <Link to="/">Dolorsit</Link>
+        //         </li>
+        //     </ul>
+        //   </nav>
+        <Nav className="" bsStyle="pills" activeKey={1} >
+            <NavItem className="navBrandLogo" eventKey={1} href="/home">
+                <a className="navbar-brand " href="#">
+                    <img className="imgLogo" src={logo}  />
+                </a>
+            </NavItem>
+            <NavItem className="navItem" eventKey={2} title="Item">
+            Summary
+            </NavItem>
+            <NavItem className="navItem" eventKey={3} disabled>
+            Journeys
+            </NavItem>
+        </Nav>
         );
     }
 }
