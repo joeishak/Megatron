3cf5a15b7e0d7dbfc76c546e8940871002fb9ac2
Joe Ishak <jishak@infosol.com>
2018-08-17 12:37:53 -0700

    Added Bottom Summary Box

---
+++ src/components/BottomSummaryBox/BottomSummaryBox.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class ButtomSummaryBox extends Component {
+    render(){
+        return(
+
+            <div className="row">
+                <div className=" col-xs-12 col-s-12 col-md-6 co-lg-3">
+
+                </div>
+                <div className=" col-xs-12 col-s-12 col-md-6 co-lg-3">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default (ButtomSummaryBox)
