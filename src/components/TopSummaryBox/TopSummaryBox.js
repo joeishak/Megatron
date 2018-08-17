c3dcc9e364c464449b34b58cb11dca4546f0980e
jrcamilon <jrcamilon@gmail.com>
2018-08-16 21:05:51 -0700

    Moved animation to boxes

--- src/components/TopSummaryBox/TopSummaryBox.js
+++ src/components/TopSummaryBox/TopSummaryBox.js
@@ -44,17 +44,61 @@ class TopSummaryBox extends Component {
     }
   
    
-    ShowCharts(){
+    ShowCharts () {
         this.timeout =  setTimeout(() => { 
         this.setState({ show: !this.state.show});
-        }, 1000);
+        }, 500);
+
+        // this.setState({ show: true});
     }
     render(){
         
-        const chart1 = this.state.show ? (<KendoDonutChart donutCenterRender= {()=> <div><h3>$149.9M</h3><span>TARGET</span><h6>$277.9M</h6></div>}/> ) : null;
-        const chart2 = this.state.show ? (<KendoDonutChart donutCenterRender= {()=> <div><h3>$335.3M</h3><span>TARGET</span><h6>$277.9M</h6></div>} />) : null;
-        const chart3 = this.state.show ? (<KendoDonutChart donutCenterRender= {()=> <div><h3>$217.5M</h3><span>TARGET</span><h6>$277.9M</h6></div>} />) : null;
-        const chart4 = this.state.show ? (<KendoDonutChart donutCenterRender= {()=> <div><h3>$32.1M</h3><span>TARGET</span><h6>$277.9M</h6></div>} />) : null;
+        const chart1 = this.state.show ? ( 
+        <div style={inStyles.sumChartSqaure} >
+            <div style={inStyles.sumChartContent}>
+                <div style={inStyles.sumChartHeader} >
+                Net New ARR
+                </div>
+                    <KendoDonutChart donutCenterRender= {()=> 
+                    <div><h3>$149.9M</h3><span>TARGET</span><h6>$277.9M</h6></div>}/> 
+                </div>
+        </div>) : null;
+        const chart2 = this.state.show ? ( 
+        <div style={inStyles.sumChartSqaure} >
+            <div style={inStyles.sumChartContent}>
+            <div style={inStyles.sumChartContent}>
+                    <div style={inStyles.sumChartHeader} >
+                    Gross New ARR
+                    </div>
+                    <KendoDonutChart donutCenterRender= {()=> 
+                    <div><h3>$159.9M</h3><span>TARGET</span><h6>$277.9M</h6></div>}/> 
+                </div>
+                </div>
+            </div>) : null;
+        const chart3 = this.state.show ? (
+        <div style={inStyles.sumChartSqaure} >
+            <div style={inStyles.sumChartContent}>
+            <div style={inStyles.sumChartContent}>
+                    <div style={inStyles.sumChartHeader} >
+                    Cancellations ARR
+                    </div>
+                    <KendoDonutChart donutCenterRender= {()=> 
+                    <div><h3>$217.5M</h3><span>TARGET</span><h6>$277.9M</h6></div>} />
+                </div>
+                </div>
+            </div>) : null;
+        const chart4 = this.state.show ? (                      
+        <div style={inStyles.sumChartSqaure} >
+            <div style={inStyles.sumChartContent}>
+            <div style={inStyles.sumChartContent}>
+                    <div style={inStyles.sumChartHeader} >
+                    Renewel@FP ARR
+                    </div>
+                    <KendoDonutChart donutCenterRender= {()=> <div><h3>$32.1M</h3><span>TARGET</span><h6>$277.9M</h6></div>} />
+                </div>
+                </div>
+            </div>) : null;
+
         return(
 
             <Grid className="gridContainer" fluid>
@@ -73,65 +117,17 @@ class TopSummaryBox extends Component {
                     </Col> */}
                 </Row>
                 <Row className="chartRow">
-                
                     <Col  xs={12} s={12} md={6} lg={3} onClick = {this.props.handleSummaryClick} >
-                      
-                        <div style={inStyles.sumChartSqaure} >
-                            <div style={inStyles.sumChartContent}>
-                                <div style={inStyles.sumChartHeader} >
-                                Net New ARR
-                                </div>
-                                <Slide style={inStyles.slideContainer}>
-                                {chart1}
-                                </Slide>
-                                
-                            </div>
-                        </div>
-
+                      <Slide transitionEnterDuration="500">{chart1}</Slide>
                     </Col>
                     <Col  xs={12} s={12} md={6} lg={3} onClick = {this.props.handleSummaryClick}>
-                        <div style={inStyles.sumChartSqaure} >
-                        <div style={inStyles.sumChartContent}>
-                        <div style={inStyles.sumChartContent}>
-                                <div style={inStyles.sumChartHeader} >
-                                Gross New ARR
-                                </div>
-                                <Slide style={inStyles.slideContainer}>
-                                {chart2}
-                                </Slide>
-
-                            </div>
-                            </div>
-                        </div>
+                      <Slide transitionEnterDuration="800">{chart2}</Slide>
                     </Col>
                     <Col  xs={12} s={12} md={6} lg={3} onClick = {this.props.handleSummaryClick}>
-                        <div style={inStyles.sumChartSqaure} >
-                        <div style={inStyles.sumChartContent}>
-                        <div style={inStyles.sumChartContent}>
-                                <div style={inStyles.sumChartHeader} >
-                                Cancellations ARR
-                                </div>
-                                <Slide style={inStyles.slideContainer}>
-                                {chart3}
-                                </Slide>
-
-                            </div>
-                            </div>
-                        </div>
+                      <Slide transitionEnterDuration="1100">{chart3}</Slide>
                     </Col>
                     <Col xs={12} s={12} md={6} lg={3} onClick = {this.props.handleSummaryClick}>
-                        <div style={inStyles.sumChartSqaure} >
-                        <div style={inStyles.sumChartContent}>
-                        <div style={inStyles.sumChartContent}>
-                                <div style={inStyles.sumChartHeader} >
-                                Renewel@FP ARR
-                                </div>
-                                <Slide style={inStyles.slideContainer}>
-                                {chart4}
-                                </Slide>
-                            </div>
-                            </div>
-                        </div>
+                      <Slide transitionEnterDuration="1400">{chart4}</Slide>
                     </Col>
                 </Row>
             </Grid>
