514c82724707b27211619cb334a2773cc0388857
Joseph Ishak <jishak1@asu.edu>
2018-08-17 16:07:18 -0700

    Fixed conflict

--- src/components/BottomSummaryBox/BottomSummaryBox.js
+++ src/components/BottomSummaryBox/BottomSummaryBox.js
@@ -17,23 +17,64 @@ class ButtomSummaryBox extends Component {
             <div className="" >
                 <div className="row gridRow1">
                     <div className = "titleHeader col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                         Net New ARR
+                        <CSSTransitionGroup
+                        className="header"
+                            transitionName="header"
+                            transitionAppear={true}
+                            transitionAppearTimeout={200}
+                            transitionEnter={false} 
+                            transitionLeave={false} onClick = {this.props.handleSummaryClick}>
+                            <span>
+                            Net New ARR
+                            </span>
+                        </CSSTransitionGroup>
+
+                         <CSSTransitionGroup
+                            transitionName="example"
+                            transitionAppear={true}
+                            transitionAppearTimeout={800}
+                            transitionEnter={false} 
+                            transitionLeave={false} onClick = {this.props.handleSummaryClick}>
                          <div className="statsHeader"> 
                             <span className="green"> -2.9%</span><br/>
                             <span> Y/Y </span>
                         </div>
+                        </CSSTransitionGroup>
+                        <CSSTransitionGroup
+                            transitionName="example"
+                            transitionAppear={true}
+                            transitionAppearTimeout={600}
+                            transitionEnter={false} 
+                            transitionLeave={false} onClick = {this.props.handleSummaryClick}>
                         <div className="statsHeader"> 
                             <span className="red"> -17.0%</span><br/>
                             <span> Q/Q LY </span>
                         </div>
+                        </CSSTransitionGroup>
+
+                           <CSSTransitionGroup
+                            transitionName="example"
+                            transitionAppear={true}
+                            transitionAppearTimeout={400}
+                            transitionEnter={false} 
+                            transitionLeave={false} onClick = {this.props.handleSummaryClick}>
                         <div className="statsHeader"> 
                             <span className="red"> -20.4%</span><br/>
                             <span> Q/Q TY </span>
                         </div>
+                        </CSSTransitionGroup>
+
+                           <CSSTransitionGroup
+                            transitionName="example"
+                            transitionAppear={true}
+                            transitionAppearTimeout={200}
+                            transitionEnter={false} 
+                            transitionLeave={false} onClick = {this.props.handleSummaryClick}>
                         <div className="statsHeader"> 
                             <span className="red"> -2.8%</span><br/>
                             <span> vs QRF </span>
                         </div>
+                        </CSSTransitionGroup>
                     
                     
                    
