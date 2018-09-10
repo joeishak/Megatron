76068f87b78544db00e0f1ea6a118ab57ffc69b7
jrcamilon <jrcamilon@gmail.com>
2018-09-10 15:33:31 -0700

    Data Preferences page updates active filters state

--- src/components/KendoDialog/Components/ReactSelect.js
+++ src/components/KendoDialog/Components/ReactSelect.js
@@ -4,8 +4,9 @@ import Select from 'react-select'
 
 // Transform the list so that the component can render the drop downs
 let transformList = (objList) => {
+
     let newObjList = [];
-    if (objList !== undefined) {
+    if (objList !== undefined && objList !== []) {
         
         // Iterate through each list of objects and convert
         for (let i = 0; i < objList.length; i++) {
@@ -18,16 +19,32 @@ let transformList = (objList) => {
                 }
             )
         }
+
     }
-    console.log(newObjList);
     return newObjList;
 };
 
+let transformList2 = (objList) => {
+
+    if ( objList !== undefined) {
+        if (objList[0] !== undefined) {
+            // console.log('TESTING', objList[0]);
+            let newObj  = {
+                index: objList[0].index,
+                category: objList[0].category,
+                value: objList[0].value,
+                label: objList[0].value
+            };
+            return newObj; 
+        }
+    }
+}
+
 // Render Component
 const ReactSelect = (props) => {
-
     return (
-        <Select options={transformList(props.options)} value={props.defaultValue}/>
+        
+        <Select options={transformList(props.options)} defaultValue={transformList2(props.defaultValue)} onChange={props.updateFilter} />
     );
 };
 
