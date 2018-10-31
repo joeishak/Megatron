77694e98c62104a39a53561ea448a8bfc66f0693
joeishak <jishak@infosol.com>
2018-10-31 17:25:02 +0000

    conditional for no multivalue settings set

--- src/reducers/userSettings.js
+++ src/reducers/userSettings.js
@@ -12,11 +12,29 @@ export default   function(state = {},action) {
     switch(action.type) {
         case GET_USER_SETTINGS:
         newState  = action.payload.data[0];
-        newState.geoFilters = JSON.parse(newState.geoFilters)
-        newState.productFilters = JSON.parse(newState.productFilters)
-        newState.routeFilters = JSON.parse(newState.routeFilters)
-        newState.subscriptionFilters = JSON.parse(newState.subscriptionFilters)
-        newState.marketFilters = JSON.parse(newState.marketFilters)
+        if(newState.geoFilters !== ""){
+            newState.geoFilters = JSON.parse(newState.geoFilters)
+
+        }
+        if(newState.productFilters !== ""){
+            newState.productFilters = JSON.parse(newState.productFilters)
+
+        }
+        if(newState.routeFilters!== ""){
+            newState.routeFilters = JSON.parse(newState.routeFilters)
+
+        } 
+        if (newState.routeFilters !== ""){
+
+        }
+        if(newState.subscriptionFilters!== ""){
+            newState.subscriptionFilters = JSON.parse(newState.subscriptionFilters)
+
+        }
+        if(newState.marketFilters !== ""){
+            newState.marketFilters = JSON.parse(newState.marketFilters)
+
+        }
             return newState;
         case UPDATE_USER_SETTINGS:
 
