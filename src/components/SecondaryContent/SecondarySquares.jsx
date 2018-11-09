808f22550abc4b21e56924a0693613456eeddc3a
joeishak <jishak@infosol.com>
2018-11-09 18:04:39 +0000

    Moved getColor Here

--- src/components/SecondaryContent/SecondarySquares.jsx
+++ src/components/SecondaryContent/SecondarySquares.jsx
@@ -5,14 +5,45 @@ import commentIconOff from '../../assets/images/comments-off.svg';
 import KendoBulletChart from '../KendoBullet/KendoBullet';
 import styles from './SecondaryContent.css';
 class SecondarySquares extends Component {
+    // Need to Refactor
+    getColor(value, target, type, header) {
+        let retColor = '';
+        if(type === 'financial' ) {
+            if (value >= target) {
+                retColor = 'selectedCardHeaderGreen';
+            } else {
+                retColor = 'selectedCardHeaderRed';
+            }
+        } else if (type === 'journey' && header === false) {
+            if (value >= target) {
+                retColor = 'journeyBoxAlertGreen';
+            } else {
+                retColor = 'journeyBoxAlert';
+            }
+        } else if (type === 'journey' && header !== false) {
+            if (value >= target) {
+                retColor = 'journeyHeaderAlertGreen';
+            } else {
+                retColor = 'journeyHeaderAlert';
+            }
+        } else if (type === 'donut') {
+            if (value < target) {
+                retColor = '#FF0000';
+            } else {
+                retColor = '#0DB16E';
+            }
+        }
+
+        return retColor;
+    }
     render(){
         return(
     <div className="journeyBoxHover" key={this.props.item.index}>    
              
-                    <div className={ `journeyBox  ${this.props.activeJourneyCard === true ? this.props.getColor(this.props.item.value, this.props.item.target, 'journey', false) : ''}`} 
+                    <div className={ `journeyBox  ${this.props.activeJourneyCard === true ? this.getColor(this.props.item.value, this.props.item.target, 'journey', false) : ''}`} 
                     onClick={e => this.props.onJourneyCardClicked(e, this.props.item.index)}>
                 
-                    <div  className={`journeyHeader k-float-left ${this.props.activeJourneyCard === true ? this.props.getColor(this.props.item.value, this.props.item.target, 'journey', true) : ''}`} >
+                    <div  className={`journeyHeader k-float-left ${this.props.activeJourneyCard === true ? this.getColor(this.props.item.value, this.props.item.target, 'journey', true) : ''}`} >
                         {/* <div className={this.props.item.css[2]}><p className="journeyHeaderTitle ">{this.props.item.title}</p></div> */}
                     </div>
 
