663e72a4d98daebdd56721cc0d07a96b2c8af559
unknown <jishak@infosol.com>
2019-02-21 12:31:41 -0700

    rebase

--- src/reducers/toggleCommentary.js
+++
@@ -1,9 +0,0 @@
-import {TOGGLE_COMMENT_CARD_VISIBILITY} from 'actions/types'
-export default function(state = true,action) {
-    switch(action.type) {
-        case TOGGLE_COMMENT_CARD_VISIBILITY:
-            return action.payload;
-        default: 
-            return state;
-    }
-}
