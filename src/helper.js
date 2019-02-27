e2861f3125a61d7a90f6f59c86c3936b2cfe3746
unknown <jishak@infosol.com>
2019-02-27 09:48:22 -0700

    Merging

--- src/helper.js
+++ src/helper.js
@@ -1,18 +1,12 @@
 import { Security, ImplicitCallback } from '@okta/okta-react';
-import { access } from 'fs';
 
 async function checkAuthentication() {
   const authenticated = await this.props.auth.isAuthenticated();
   if (authenticated !== this.state.authenticated) {
     if (authenticated && !this.state.userinfo) {
       const userinfo = await this.props.auth.getUser();
-      const accessToken = await this.props.auth.getAccessToken();
 
-      this.setState({ authenticated, userinfo }, () => {
-        console.log(userinfo);
-        console.log(accessToken);
-
-      });
+      this.setState({ authenticated, userinfo });
       if (this.props.user.sub === undefined) {
         this.props.updateOKTAUser(userinfo);
         // this.props.getUserSettings(this.props.user.sub);
