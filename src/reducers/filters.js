a7fdbb034a547e63fc4b0bfdaaac05947e11ee88
unknown <jishak@infosol.com>
2019-05-29 08:58:10 -0700

    Saved one more file

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -442,7 +442,6 @@ export default function (state = {
                             }
                         }
                     }
-                  
                 }
             })
             return {...copyOfState, isDefaultFilters: false, filtersAreDefault: false};
